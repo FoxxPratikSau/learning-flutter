c0f61a4e3b06d1d416c3a62171cf8dfbb24d8b08
FoxxPratikSau <pratik.sau09@gmail.com>
2022-04-12 23:38:06 +0530

    change 1.3

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -8,13 +8,13 @@ class Myapp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-   
+   int days =30;
 
     return MaterialApp(
       home: Material(
         child: Center(
           child: Container(
-            child: Text("hello world"),
+            child: Text("hello   world"),
           ),
         ),
       ),
